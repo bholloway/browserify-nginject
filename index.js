9daebe6a0d02a1baa3aa594d7729f36e7af7ef35
benholloway <ben.holloway@3plearning.com>
2015-04-20 21:53:13 +1000

    added inference of angular entities, removed shrinkwrap

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 var esprimaTools = require('browserify-esprima-tools');
 
+var testNode     = require('./lib/ast-tests'),
+    inferAngular = require('./lib/infer-angular'),
+    processNode  = require('./lib/process-node');
+
 /**
  * Esprima based explicity @ngInject annotation with sourcemaps.
  * @param {object} opt An options hash
@@ -21,8 +25,10 @@ module.exports = browserifyNgInject;
 function updater(file, ast) {
   if (ast.comments) {
     ast.comments
-      .filter(testNgInject)
+      .filter(testDocTag)
       .map(getAnnotatedNode)
+      .concat(inferAngular(ast))    // find the items that are not explicitly annotated
+      .filter(testFirstOccurrence)  // ensure unique values
       .forEach(processNode);
   } else {
     throw new Error('Esprima AST is required to have top-level comments array');
@@ -34,7 +40,7 @@ function updater(file, ast) {
  * Test the comment content for the <code>@ngInject</code> doctag.
  * @param {object} comment The comment node
  */
-function testNgInject(comment) {
+function testDocTag(comment) {
   return /@ngInject/i.test(comment.value);
 }
 
@@ -51,7 +57,7 @@ function getAnnotatedNode(comment) {
     var children = esprimaTools.orderNodes(comment.annotates);
     result = [comment.annotates]
       .concat(children)
-      .filter(testFunctionType)
+      .filter(testNode.isFunction)
       .shift();
   }
 
@@ -59,77 +65,16 @@ function getAnnotatedNode(comment) {
   if (result) {
     return result;
   } else {
-    throw new Error('Doctag @ngInject does not annotate anything');
+    throw new Error('Doc-tag @ngInject does not annotate anything');
   }
 }
 
 /**
- * Test whether the given esprima node is a function declaration or expression node.
- * @param {{type:string}} node An esprima AST node to test
- * @returns {boolean} True on match, else False
+ * Test whether the given value is the first occurance in the array.
+ * @param {*} value The value to test
+ * @param {number} i The index of the value in the array
+ * @param {Array} array The array the value is within at the given index
  */
-function testFunctionType(node) {
-  return /^Function(Declaration|Expression)$/.test(node.type);
-}
-
-/**
- * Add explicit dependency statements to the node.
- * @param {object} node An esprima AST function-type node
- */
-function processNode(node) {
-
-  // check if the function is part of a variable assignment
-  var isVarAssignment = (node.parent.type === 'VariableDeclarator');
-
-  // the parameters of the function, converted to literal strings
-  var params = node.params.map(paramToLiteral);
-
-  // [ 'arg', ..., function(arg) {} ]
-  if ((node.type === 'FunctionExpression') && !isVarAssignment) {
-    esprimaTools.nodeSplicer(node)({
-      parent  : node.parent,
-      type    : 'ArrayExpression',
-      elements: params.concat(node)
-    });
-  }
-
-  // fn.$inject = [ 'arg', ... ]
-  else {
-    var appendTo = isVarAssignment ? node.parent.parent : node;
-    esprimaTools.nodeSplicer(appendTo, +1)({
-      type      : 'ExpressionStatement',
-      expression: {
-        type    : 'AssignmentExpression',
-        operator: '=',
-        left    : {
-          type    : 'MemberExpression',
-          computed: false,
-          object  : {
-            type: 'Identifier',
-            name: node.id.name
-          },
-          property: {
-            type: 'Identifier',
-            name: '$inject'
-          }
-        },
-        right   : {
-          type    : 'ArrayExpression',
-          elements: params
-        }
-      }
-    });
-  }
-}
-
-/**
- * Clone a simple nodes with type and name.
- * @param {{type:string, name:string}} node The node to clone
- * @returns {{type:string, name:string}} A copy of the original node
- */
-function paramToLiteral(node) {
-  return {
-    type : 'Literal',
-    value: node.name
-  };
+function testFirstOccurrence(value, i, array) {
+  return (array.indexOf(value) === i);
 }
